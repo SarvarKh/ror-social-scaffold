d342a309c37df124c233f2bd2aa4649e52370edd
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-03-24 15:58:19 +0500

    Correct FR invitation and acceptance logic

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -8,10 +8,7 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.new(friendship_params)
     
-    user_second = User.find(params[:friend_id])
-    @friendship_second = user_second.friendships.new(user_id: params[:friend_id], friend_id: params[:user_id], confirmed: params[:confirmed])
-
-    if @friendship.save && @friendship_second.save
+    if @friendship.save
       redirect_to users_path, notice: 'You sent a friend request.'
     else
       redirect_to users_path, alert: 'You already sent a friend request.'
@@ -19,10 +16,12 @@ class FriendshipsController < ApplicationController
   end
 
   def update
+    user_second = User.find(params[:user_id])
+    @friendship_second = user_second.friendships.new(friend_id: params[:friend_id], user_id: params[:user_id], confirmed: true)
+    @friendship_second.save
+
     friendship = Friendship.find_by(friend_id: current_user, user: params[:friend_id])
-    friendship_second = Friendship.find_by(user_id: current_user.id, friend_id: params[:friend_id])
     friendship.update(confirmed: true)
-    friendship_second.update(confirmed: true)
     redirect_to user_friendships_path, notice: 'You accepted a friend request.'
   end
 
